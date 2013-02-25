a101d8e5f9d3aa89fb5651d5da519235b46ecffa
cropinfo <cropinformatics@gmail.com>
2013-02-25 22:29:39 +0100

    added smaller test data and cached listener

---
+++ corehunter-lib/src/test/java/org/corehunter/test/search/impl/CachedSolutionPrintWriterSubsetSearchListener.java
@@ -0,0 +1,37 @@
+package org.corehunter.test.search.impl;
+
+import java.util.HashMap;
+
+import org.corehunter.CoreHunterException;
+import org.corehunter.search.Search;
+import org.corehunter.search.SearchListener;
+import org.corehunter.search.impl.PrintWriterSubsetSearchListener;
+import org.corehunter.search.solution.SubsetSolution;
+
+public class CachedSolutionPrintWriterSubsetSearchListener<IndexType, SolutionType extends SubsetSolution<IndexType>> extends PrintWriterSubsetSearchListener<IndexType, SolutionType>
+{
+	HashMap<SolutionType, Double> solutions = new HashMap<SolutionType, Double>() ;
+	
+	@Override
+  public void newBestSolution(Search<SolutionType> search,
+      SolutionType bestSolution, double bestSolutionEvaluation)
+  {
+	  if (solutions.containsKey(bestSolution))
+	  {
+	  	if (solutions.get(bestSolution) == bestSolutionEvaluation)
+	  		getPrintStream().println("*** Existing solution with same evaluation for: " + search.getName() + " evaluation: "  + bestSolutionEvaluation + " size: " + bestSolution.getSubsetSize() + " solution: " + bestSolution) ;
+	  	else
+	  		getPrintStream().println("*** Existing solution with different evaluation for: " + search.getName() + " new evaluation: "  + bestSolutionEvaluation + " previous evaluation: "  + solutions.get(bestSolution) + " size: " + bestSolution.getSubsetSize() + " solution: " + bestSolution) ;
+	  }
+	  else
+	  {
+		  super.newBestSolution(search, bestSolution, bestSolutionEvaluation);
+		  
+	  	solutions.put(bestSolution, bestSolutionEvaluation) ;
+	  }
+  }
+
+
+
+
+}
