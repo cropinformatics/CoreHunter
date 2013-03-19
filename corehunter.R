0aad00f38284fdc323348035a02439eea03e29cd
Herman De Beukelaer <herman.debeukelaer@gmail.com>
2013-03-19 18:03:18 +0100

    Updated R script

--- corehunter.R
+++ corehunter.R
@@ -19,7 +19,7 @@ corehunter.run <- function(options, mem="512m"){
         stop("Core Hunter CLI jar file not found.")
     }
     # run CLI
-    mempar = paste("-Xmx", mem, sep="");
+    mempar = paste("-Xmx", mem, sep="")
     system(paste("java", mempar, "-jar", cli, options))
 }
 
@@ -40,6 +40,37 @@ coreanalyser.run <- function(options, mem="512m"){
         stop("Core Analyser CLI jar file not found.")
     }
     # run CLI
-    mempar = paste("-Xmx", mem, sep="");
-    system(paste("java", mempar, "-jar", cli, options))
+    mempar = paste("-Xmx", mem, sep="")
+    output = system(paste("java", mempar, "-jar", cli, options), intern=TRUE)
+    # remove head and tail from output
+    measures = output[2];
+    output = output[3:(length(output)-4)]
+    # parse measures
+    measures = gsub("^\\s+|\\s+$", "", measures)
+    measures = strsplit(measures, "\\s+")[[1]]
+    # create results data frame
+    results = data.frame()
+    # parse dataset evaluations and fill data frame
+    for(i in 1:length(output)){
+        # get dataset name
+        tmp = gregexpr(":", output[i])[[1]]
+        tmp = tmp[length(tmp)]
+        dataset = substr(output[i], 1, tmp-1)
+        # get evaluations
+        evaluations = substr(output[i], tmp+1, nchar(output[i]))
+        evaluations = gsub("^\\s+|\\s+$", "", evaluations)
+        evaluations = strsplit(evaluations, "\\s+")[[1]]
+        # store in results data frame
+        for(m in 1:length(measures)){
+            results[dataset, measures[m]] = evaluations[m];
+        }
+    }
+    #return results
+    return(results);
 }
+
+
+
+
+
+
